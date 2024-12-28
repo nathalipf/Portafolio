b2572654015aa3742e75ba45f10841a570e0df0c
Nathali Perez <nathalipf2803@gmail.com>
2024-12-28 12:48:14 -0500

    Latest update in Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+    <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+        <i class='bx bxl-instagram'></i>
+    </a>
+
+    <a href="https://dribbble.com/" className="home__social-icon" target="_blank"> 
+        <i class='bx bxl-dribbble' ></i>
+    </a>
+
+    <a href="https://github.com/" className="home__social-icon" target="_blank">  
+        <i class='bx bxl-github' ></i>
+    </a>
+    </div>
+  )
+}
+
+export default Social
