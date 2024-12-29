437708c706a43797e1239ea82ac802704ee94493
Nathali Perez <nathalipf2803@gmail.com>
2024-12-29 15:54:28 -0500

    Skills Component Updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 
 
@@ -14,6 +15,7 @@ const App = () => {
     <main className='main'>
       <Home />
       <About />
+      <Skills />
     </main>
     </>
   )
