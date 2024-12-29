73caeb121bd510c0e84e3c8571135c565448a174
Nathali Perez <nathalipf2803@gmail.com>
2024-12-29 13:56:01 -0500

    First update in Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+        <div className="about__box">
+            <h3 className="about__tiitle">Experience</h3>
+            <span className="about__subtitle">8 Years Working</span>
+        </div>
+
+        <div className="about__box">
+            <h3 className="about__tiitle">Completed</h3>
+            <span className="about__subtitle">48 + Projects</span>
+        </div>
+
+        <div className="about__box">
+            <h3 className="about__tiitle">Support</h3>
+            <span className="about__subtitle">Online 24/7</span>
+        </div>
+
+    </div>
   )
 }
 
