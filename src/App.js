9e99184fd9176361c7d55918bf07f80762e88e43
Nathali Perez <nathalipf2803@gmail.com>
2024-12-29 13:49:08 -0500

    About First Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./App.css";
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
 
     <main className='main'>
       <Home />
+      <About />
     </main>
     </>
   )
