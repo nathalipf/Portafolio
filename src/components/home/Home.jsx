553df3a730aab0e05ff517dc85d96c9cd0d3c537
Nathali Perez <nathalipf2803@gmail.com>
2024-12-28 21:03:19 -0500

    Last commit Home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./home.css";
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
   return (
@@ -16,6 +17,8 @@ const Home = () => {
 
                 <Data />
             </div>
+
+            <ScrollDown />
         </div>
     </section>
   )
