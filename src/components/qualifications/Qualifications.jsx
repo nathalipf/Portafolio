af107600eaaf24eabe8d716b7136eaae26e178ff
Nathali Perez <nathalipf2803@gmail.com>
2025-01-04 12:24:06 -0500

    New component Testimonials

--- src/components/qualifications/Qualifications.jsx
+++ src/components/qualifications/Qualifications.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./qualifications.css";
 
 const Qualifications = () => {
@@ -16,12 +16,12 @@ const Qualifications = () => {
 
             <div className="qualification__container container">
                 <div className="qualification__tabs">
-                    <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}>
+                    <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(1)}>
                         <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
                         Education
                     </div>
 
-                    <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"}>
+                    <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button button--flex"} onClick={() => toggleTab(2)}>
                         <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
                         Experience
                     </div>
@@ -94,7 +94,7 @@ const Qualifications = () => {
                         </div>
                     </div>
 
-                    <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                         <div className="qualification__data">
                             <div>
                                 <h3 className="qualification__title">Product Designer</h3>
