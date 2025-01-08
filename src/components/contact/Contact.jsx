0276c4a6109f5a68e5c139f204fff24f3acd6f54
Nathali Perez <nathalipf2803@gmail.com>
2025-01-07 22:37:43 -0500

    Update contact.css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -80,7 +80,7 @@ const Contact = () => {
               />
             </div>
 
-            <div className="contact__form-div">
+            <div className="contact__form-div contact__form-area">
               <label className="contact__form-tag">Project</label>
               <textarea
                 name="project"
