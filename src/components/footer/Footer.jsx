6f33218aacb900852d5306d2c815da811117dc38
Nathali Perez <nathalipf2803@gmail.com>
2025-01-18 13:12:13 -0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-   <footer classname="footer">
-        <div classname="footer__container container">
-            <h1 classname="footer__title">Nathali</h1>
+    <footer classname="footer">
+      <div classname="footer__container container">
+        <h1 classname="footer__title">Nathali</h1>
 
-            <ul classname="footer__list">
-                <li>
-                    <a href="#home" classname="footer__link"></a>
-                </li>
-                
-            </ul>
+        <ul classname="footer__list">
+          <li>
+            <a href="#about" classname="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#portfolio" classname="footer__link">
+              Projects
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" classname="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div classname="footer__social">
+          <a
+            href="https://www.facebook.com/"
+            className="footer__social-link"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i class="bx bxl-facebook"></i>
+          </a>
+
+          <a
+            href="https://www.instagram.com/"
+            className="footer__social-link"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://www.twitter.com/"
+            className="footer__social-link"
+            target="_blank" rel="noopener noreferrer"
+          >
+            <i class="bx bxl-twitter"></i>
+          </a>
         </div>
-   </footer>
-  )
-}
 
-export default Footer
+        <span classname="footer__copy">&#169;TechNathSolutions. All rigths reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
