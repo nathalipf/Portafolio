c95e891d6c0140b0a0de0d6243164d8b6b7d2818
Nathali Perez <nathalipf2803@gmail.com>
2025-01-20 18:26:33 -0500

    Scrollup updates

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './scrollup.css';
 
-const Scrollup = () => {
+const ScrollUp = () => {
     window.addEventListener('scroll', function() {
-        const scrollup = document.querySelector('.scrollup');
-        if (this.scrollY >= 560) scrollup.classList.add('show-scroll');
-        else scrollup.classList.remove('show-scroll');    
+        const scrollUp = document.querySelector('.scrollup');
+        if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+        else scrollUp.classList.remove('show-scroll');    
     });
   return (
     <a href="#" className="scrollup">
@@ -14,4 +14,4 @@ const Scrollup = () => {
   )
 }
 
-export default Scrollup
+export default ScrollUp
